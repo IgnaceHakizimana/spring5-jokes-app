58a51fd9d1d4c68f9b7ba9bf28e03aa6d04c251b
Ignace <ignace.hakizimana@aos.rw>
2020-07-26 10:33:56 +0200

    Use XML configuration

--- src/main/java/guru/springframework/joke/spring5jokesapp/Spring5JokesappApplication.java
+++ src/main/java/guru/springframework/joke/spring5jokesapp/Spring5JokesappApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.joke.spring5jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class Spring5JokesappApplication {
 
 	public static void main(String[] args) {
